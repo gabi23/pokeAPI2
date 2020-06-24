8c1761ced91f25ae27a5881c152534d202251108
gabi23 <46545624+CloudyAsteroid@users.noreply.github.com>
2020-06-24 19:54:17 +0000

    Se incluye la ruta hacia el componente GenerationComponent, en función de la id

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GenerationComponent } from './components/generation/generation.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'generation', redirectTo: 'generation/1', pathMatch: 'full' },
+  /* {path: 'generation/:id', component: GenerationComponent } */
+  {path: 'generation/:id', component: GenerationComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
