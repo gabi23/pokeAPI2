7c9021bb30b4c8f1b0db2f76ee2057610af406dd
gabi23 <gabi2332@gmail.com>
2020-06-27 20:24:03 +0000

    Se añade una ruta para navegar a los detalles de un pokemon

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { GenerationComponent } from './components/generation/generation.component';
+import { PokemonDetailsComponent } from './components/pokemon-details/pokemon-details.component';
 
 
 const routes: Routes = [
-  {path: 'generation', redirectTo: 'generation/1', pathMatch: 'full' },
-  {path: 'generation/:id', component: GenerationComponent }
+  {path: '', redirectTo: 'generation/1', pathMatch: 'full' },
+  {path: 'generation/:id', component: GenerationComponent },
+  {path: 'generation/:id/:name', component: PokemonDetailsComponent }
 ];
 
 @NgModule({
