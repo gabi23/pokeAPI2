467ff33c07acae016313acfc60fe8ae1620d8e88
gabi23 <gabi2332@gmail.com>
2020-06-27 20:27:26 +0000

    Se crea el componente 'pokemon-details'
    Este componente sirve para mostrar los detalles o características del pokemon que se elija.

--- src/app/components/pokemon-details/pokemon-details.component.ts
+++ src/app/components/pokemon-details/pokemon-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PokeapiService } from 'src/app/services/pokeapi.service';
 
 @Component({
   selector: 'app-pokemon-details',
@@ -7,9 +9,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PokemonDetailsComponent implements OnInit {
 
-  constructor() { }
+  pokemonData: any;
+  name: string = "";
+  id: string = "";  
+  height: string = "";
+  weight: string = "";
+  imageURL: string = "";
+  types: string[] = [];
+  stats: string[] = [];
+
+
+  constructor(private route: ActivatedRoute, private pokeapiServices: PokeapiService) { 
+    this.getPokemonDetails();
+  }
 
   ngOnInit(): void {
+  }
+
+  async getPokemonDetails(): Promise<void>{
+    const pokemonName = this.route.snapshot.paramMap.get('name');
+    this.pokemonData = await this.pokeapiServices.getPokemonData(pokemonName);
+ 
+    this.getBasicData();
+    this.getImageURL();
+    this.getTypes();
+    this.getStats();
+  }
+
+  getBasicData() {
+    this.name = this.pokemonData.name;
+    this.id = this.pokemonData.id;
+    this.height = this.pokemonData.height;
+    this.weight = this.pokemonData.weight;
+
+    console.log(this.name);
+    console.log(this.id)
+  }
+
+  getImageURL() {
+    this.imageURL = `https://pokeres.bastionbot.org/images/pokemon/${this.id}.png`
+  }
+
+  getTypes() {
+    this.types = this.pokemonData.types.map(type => type.type.name);
+  }
+
+  getStats() {
+    this.stats = this.pokemonData.stats.map(stat => stat.base_stat);
   }
 
 }
